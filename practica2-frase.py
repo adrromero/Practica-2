3f2cac4e991880a6e1c7ad32436091de7803b9d1
adrromero <118919854+adrromero@users.noreply.github.com>
2022-12-07 16:50:31 +0100

    Create practica2-frase.py
    practica2 enunciado y donde se hara el ejercicio

---
+++ practica2-frase.py
@@ -0,0 +1,25 @@
+"""Amb el teu company, clona un mateix repositori per treballar de manera conjunta:
+
+    Afegeix a l'alumne 2 com a col·laborador al teu projecte
+    Crear un fitxer de python que mostri un menú de selecció amb 3 opcions:
+
+    Introdueix una frase
+    Elimina les vocals
+    Elimina les consonants
+    Ordena la frase al revés i printala per pantalla
+    Ordena les paraules de la frase en orde ascendent i printales per pantalla
+    Des d'aquest moment, pugeu els canvis al repositori.
+    L'alumne 1 treballarà en una branca nova anomenada “vocals” i desenvoluparà les funcions 1, 2 i 4.
+    L'alumne 2 treballarà en una branca nova anomenada “consonants” i desenvoluparà les funcions 3 i 5.
+    El projecte final haurà de fer funcionar tot conjuntament
+    Afegeix un fitxer Readme.md el més personalitzat possible.
+
+    Mínim 3 commits per usuari.
+
+Fes un petit tutorial en PDF amb captures de què has fet i com justificant tot. (1 per parella).
+
+Adjunta al tutorial la URL del repositori."""
+
+
+
+
