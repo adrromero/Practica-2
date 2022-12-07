55a16f371cdf3b9c79eec2a7ad9e5577318df7e5
adrianromero <adrromero.cf@iesesteveterradas.cat>
2022-12-07 17:01:08 +0100

    codigo actualizado con los numeros

--- practica2-frase.py
+++ practica2-frase.py
@@ -3,22 +3,23 @@
     Afegeix a l'alumne 2 com a col·laborador al teu projecte
     Crear un fitxer de python que mostri un menú de selecció amb 3 opcions:
 
-    Introdueix una frase
-    Elimina les vocals
-    Elimina les consonants
-    Ordena la frase al revés i printala per pantalla
-    Ordena les paraules de la frase en orde ascendent i printales per pantalla
-    Des d'aquest moment, pugeu els canvis al repositori.
-    L'alumne 1 treballarà en una branca nova anomenada “vocals” i desenvoluparà les funcions 1, 2 i 4.
-    L'alumne 2 treballarà en una branca nova anomenada “consonants” i desenvoluparà les funcions 3 i 5.
-    El projecte final haurà de fer funcionar tot conjuntament
-    Afegeix un fitxer Readme.md el més personalitzat possible.
+    1-Introdueix una frase
+    2-Elimina les vocals
+    3-Elimina les consonants
+    4-Ordena la frase al revés i printala per pantalla
+    5-Ordena les paraules de la frase en orde ascendent i printales per pantalla
+    6-Des d'aquest moment, pugeu els canvis al repositori.
+    7-L'alumne 1 treballarà en una branca nova anomenada “vocals” i desenvoluparà les funcions 1, 2 i 4.
+    8-L'alumne 2 treballarà en una branca nova anomenada “consonants” i desenvoluparà les funcions 3 i 5.
+    9-El projecte final haurà de fer funcionar tot conjuntament
+    10-Afegeix un fitxer Readme.md el més personalitzat possible.
 
     Mínim 3 commits per usuari.
 
 Fes un petit tutorial en PDF amb captures de què has fet i com justificant tot. (1 per parella).
 
 Adjunta al tutorial la URL del repositori."""
+
 
 
 
