f8b6c320fde7eaa86a807a643013ea3db9cf88b6
adrromero <118919854+adrromero@users.noreply.github.com>
2022-12-20 20:21:50 +0100

    Funcion sinVocales
    Se a terminado la funcion sinVocales

--- practica2-frase.py
+++ practica2-frase.py
@@ -21,42 +21,8 @@ Fes un petit tutorial en PDF amb captures de què has fet i com justificant tot.
 Adjunta al tutorial la URL del repositori."""
 
 
-def consonantesFuera(f):
+def sinVocales(f):
     vocales = ("a", "e", "i", "o", "u")
-    nueva_frase = ""
-    for i in range(len(f)):
-        if f[i].lower() in vocales:
-            nueva_frase += f[i]
-    return nueva_frase
-
-
-def ordenarPalabras(f):
-    palabras = []
-    nueva_frase = ""
-    cont = 0
-    while True:
-        if cont == 0:
-            palabras.append(f[:f.find(" ")])
-            cont += 1
-            f = f[f.find(" ") + 1:]
-        elif f.find(" ") == -1:
-            palabras.append(f)
-            break
-        else:
-            palabras.append(f[:f.find(" ")])
-            f = f[f.find(" ") + 1:]
-
-    for pas in range(len(palabras) - 1):
-        for i in range(len(palabras) - 1 - pas):
-            if palabras[i] > palabras[i + 1]:
-                cambio = palabras[i]
-                palabras[i] = palabras[i + 1]
-                palabras[i + 1] = cambio
-
-    for cadena in palabras:
-        nueva_frase += cadena + " "
-    return nueva_frase
-
-
-print(consonantesFuera("Casa Bosque Azul"))
-print(ordenarPalabras("Casa Bosque Azul"))
+    for letra in vocales:
+        f = f.replace(letra, "")
+    return f
