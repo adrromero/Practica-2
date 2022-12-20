349900ff769507554e3f5b65149d0488e1675bed
MatillaSan <dsanchezmatilla.cf@iesesteveterradas.cat>
2022-12-20 18:18:31 +0100

    Comit antes de probar funcion ordenarPalabras

--- practica2-frase.py
+++ practica2-frase.py
@@ -28,3 +28,30 @@ def consonantesFuera(f):
         if f[i].lower() in vocales:
             nueva_frase += f[i]
     return nueva_frase
+
+
+def ordenarPalabras(f):
+    palabras = []
+    nueva_frase = ""
+    cont = 0
+    while True:
+        if cont == 0:
+            palabras.append(f[:f.find(" ")])
+            cont += 1
+            f = f[f.find(" ") + 1:]
+        elif f.find(" ") == -1:
+            palabras.append(f[:f.find(" ")])
+            break
+        else:
+            palabras.append(f[:f.find(" ")])
+            f = f[f.find(" ") + 1:]
+
+    for pas in range(len(palabras) - 1):
+        for i in range(len(palabras) - 1 - pas):
+            if palabras[i] > palabras[i + 1]:
+                cambio = palabras[i]
+                palabras[i] = palabras[i + 1]
+                palabras[i + 1] = cambio
+
+    for cadena in palabras:
+        nueva_frase += cadena + " "
