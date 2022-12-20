e7860ccbec08b5357bc58189857570c7fdd71329
MatillaSan <dsanchezmatilla.cf@iesesteveterradas.cat>
2022-12-20 20:52:48 +0100

    Merge a mano porque estaba dando problemas para juntar las ramas con main

--- practica2-frase.py
+++ practica2-frase.py
@@ -21,6 +21,43 @@ Fes un petit tutorial en PDF amb captures de què has fet i com justificant tot.
 Adjunta al tutorial la URL del repositori."""
 
 
+def consonantesFuera(f):
+    vocales = ("a", "e", "i", "o", "u")
+    nueva_frase = ""
+    for i in range(len(f)):
+        if f[i].lower() in vocales:
+            nueva_frase += f[i]
+    return nueva_frase
+
+
+def ordenarPalabras(f):
+    palabras = []
+    nueva_frase = ""
+    cont = 0
+    while True:
+        if cont == 0:
+            palabras.append(f[:f.find(" ")])
+            cont += 1
+            f = f[f.find(" ") + 1:]
+        elif f.find(" ") == -1:
+            palabras.append(f)
+            break
+        else:
+            palabras.append(f[:f.find(" ")])
+            f = f[f.find(" ") + 1:]
+
+    for pas in range(len(palabras) - 1):
+        for i in range(len(palabras) - 1 - pas):
+            if palabras[i] > palabras[i + 1]:
+                cambio = palabras[i]
+                palabras[i] = palabras[i + 1]
+                palabras[i + 1] = cambio
+
+    for cadena in palabras:
+        nueva_frase += cadena + " "
+    return nueva_frase
+
+
 def sinVocales(f):
     vocales = ("a", "e", "i", "o", "u")
     for letra in vocales:
@@ -31,6 +68,10 @@ def sinVocales(f):
 frase = input("Dame una frase: ")
 
 print(sinVocales(frase.lower()))
+
+print(consonantesFuera(frase))
+
+print(ordenarPalabras(frase))
 
 frase = frase[::-1]
 print(frase)
